a1821313f154cd65c09e447cbc1819a1442e4133
Jaggy <jaggy@hey.com>
2024-03-23 01:50:48 +0800

    More inertia setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,12 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+
+    <noscript>
+        @vite('resources/css/app.pcss')
+    </noscript>
+
+    @vite('resources/js/app.ts')
     @inertiaHead
 </head>
 <body>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import vue from "@vitejs/plugin-vue"
+import laravel from "laravel-vite-plugin"
+import { defineConfig } from "vite"
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.pcss', 'resources/js/app.ts'],
+            input: ["resources/css/app.pcss", "resources/js/app.ts"],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-});
+})
